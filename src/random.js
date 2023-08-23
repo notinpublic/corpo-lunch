4c28cb971345c93f135ca5db2f1b5262af12ba26
notinpublic <susiparo1@gmail.com>
2023-08-23 22:38:39 +0100

    Moved restaurant data

--- src/random.js
+++ src/random.js
@@ -1,20 +1,7 @@
-const test = [
-  {
-    name: "Pizza",
-    location: "123 street",
-  },
-  {
-    name: "Pasta",
-    location: "456 street",
-  },
-  {
-    name: "Mandolino",
-    location: "789 street",
-  },
-];
+import { restaurantData } from './restaurant-list.js';
 
 function getRandomRestaurant(restaurants) {
   return restaurants[Math.floor(Math.random() * restaurants.length)].name;
 }
 
-console.log(getRandomRestaurant(test));
+console.log(getRandomRestaurant(restaurantData));
---
+++ src/restaurant-list.js
@@ -0,0 +1,18 @@
+export const restaurantData = [
+    {
+      name: "Pizza",
+      location: "123 street",
+    },
+    {
+      name: "Pasta",
+      location: "456 street",
+    },
+    {
+      name: "Burger",
+      location: "789 street",
+    },
+    {
+      name: "Sushi",
+      location: "sushi street"
+    }
+  ];
